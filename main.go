619b2e273ee279608e44ff6f20fd7fb7d09e6815
Dan Higham <dan.higham@gmail.com>
2018-03-29 07:21:46 -0700

    Use bigger droplet

--- main.go
+++ main.go
@@ -87,7 +87,7 @@ func main() {
 	createRequest := &godo.DropletCreateRequest{
 		Name:   dropletName,
 		Region: "lon1",
-		Size:   "4gb",
+		Size:   "8gb",
 		Image: godo.DropletCreateImage{
 			Slug: "coreos-stable",
 		},
